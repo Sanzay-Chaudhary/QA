7cf4127bcd645ab69a7daaa2e228be55de6e5a76
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-05-04 09:03:30 +0545

    created login page

--- project/tests/test_login.py
+++ project/tests/test_login.py
@@ -1,34 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from pages.login_page import LoginPage
 
-# Start browser
-driver = webdriver.Chrome()
+def test_valid_login(setup):
+    driver = setup
+    login = LoginPage(driver)
+    login.enter_username("tomsmith")
+    login.enter_password("SuperSecretPassword!")
+    login.click_login()
+    
+    assert "secure" in driver.current_url
 
-driver.get("https://the-internet.herokuapp.com/login")
+def test_invalid_login(setup):
+    driver = setup
+    login = LoginPage(driver)
+    login.enter_username("tomsmith")
+    login.enter_password("WrongPassword!")
+    login.click_login()
 
-driver.maximize_window()
-
-wait = WebDriverWait(driver, 15)
-
-print("Website opened")
-
-# Enter username
-username = wait.until(
-    EC.presence_of_element_located((By.ID, "username"))
-)
-
-username.send_keys("tomsmith")
-
-# Enter password
-password = driver.find_element(By.ID, "password")
-password.send_keys("SuperSecretPassword!")
-
-# Click login button
-login_btn = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
-login_btn.click()
-
-print("Login button clicked")
-
-driver.quit()
+    assert "invalid" in driver.page_source
