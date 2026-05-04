9411e4ac85e40354e0bdfa25e734af3c081d7089
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-05-04 11:19:26 +0545

    login setup

--- qa-automation-project/tests/test_login.py
+++ qa-automation-project/tests/test_login.py
@@ -17,3 +17,13 @@ def test_invalid_login(setup):
     login.click_login()
 
     assert "invalid" in driver.page_source
+
+def test_empty_login(setup):
+    driver = setup
+    login = LoginPage(driver)
+
+    login.enter_username("")
+    login.enter_password("")
+    login.click_login()
+
+    assert "Your username is invalid!" in driver.page_source
