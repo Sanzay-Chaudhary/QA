765d710052067dd3ba42df36adb2b3823fe3fa7d
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-05-11 22:53:13 +0545

    screenshot file

--- playwright-project/conftest.py
+++ playwright-project/conftest.py
@@ -1,11 +1,31 @@
-from playwright.sync_api import sync_playwright
 import pytest
+from playwright.sync_api import sync_playwright
+
 
 @pytest.fixture
-def page():
+def page(request):
+
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless= False)
+
+        browser = p.chromium.launch(headless=False)
+
         page = browser.new_page()
 
         yield page
+
+        # Screenshot if failed
+        if request.node.rep_call.failed:
+            page.screenshot(
+                path=f"screenshots/{request.node.name}.png"
+            )
+
         browser.close()
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+    outcome = yield
+    rep = outcome.get_result()
+
+    setattr(item, "rep_" + rep.when, rep)
