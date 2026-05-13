49ab22a0c21bc50d48c592d0cbd724a62c5c42ce
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-05-13 21:19:06 +0545

    login-page

---
+++ playwright-project/pages/login_page.py
@@ -0,0 +1,24 @@
+class LoginPage():
+    def __init__(self, page):
+        self.page = page
+
+        # Locators for the login page elements
+        self.username_input = page.locator("#username")
+        self.password_input = page.locator("#password")
+        self.login_button = page.locator("button[type= 'submit']")
+
+        self.flash_message = page.locator("#flash")
+
+    #Actions
+    def open_login_page(self):
+        self.page.goto( "https://the-internet.herokuapp.com/login")
+
+    def login(self, username, password):
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        self.login_button.click()
+
+    
+    #validations
+    def get_flash_message(self):
+        return self.flash_message.inner_text()
