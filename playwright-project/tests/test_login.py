fb4068072ee7cbf19ca6fa495a20caef42484e0c
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-05-13 21:38:07 +0545

    test-login

--- playwright-project/tests/test_login.py
+++ playwright-project/tests/test_login.py
@@ -5,6 +5,6 @@ def test_valid_login(page):
     login_page.open_login_page()
     login_page.login("tomsmith", "SuperSecretPassword!")
 
-    assert "Secure" in page.url
+    assert "secure" in page.url
 
     assert "You logged into a secure area!" in login_page.get_flash_message()
