864e51947911ec9bcea292813c31788854aaa600
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-05-14 22:22:56 +0545

    dashboard updated

--- playwright-project/pages/dashboard_page.py
+++ playwright-project/pages/dashboard_page.py
@@ -1,15 +1,22 @@
-class DashboardPage:
+from pages.base_page import BasePage
+
+
+class DashboardPage(BasePage):
 
     def __init__(self, page):
-        self.page = page
 
-        # ✅ More specific locator
-        self.logout_button = page.locator("a[href='/logout']")
+        super().__init__(page)
 
-        self.flash_message = page.locator("#flash")
+        self.logout_button = (
+            "a[href='/logout']"
+        )
+
+        self.flash_message = "#flash"
 
     def logout(self):
-        self.logout_button.click()
+
+        self.click(self.logout_button)
 
     def get_flash_message(self):
-        return self.flash_message.inner_text()
+
+        return self.get_text(self.flash_message)
