5f0e66869b952da864f83628d62478a6ee110e79
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-05-27 21:24:25 +0545

    DDT

--- playwright-project/tests/test_login_ddt.py
+++ playwright-project/tests/test_login_ddt.py
@@ -1,18 +1,37 @@
 import pytest
 from pages.login_page import LoginPage
 
+
 @pytest.mark.parametrize(
-    "username,password,expected",
+    "username, password, expected",
     [
-        ("tomsmith", "SuperSecretPassword!", "secure"),
-        ("tomsmith", "wrongpass", "Your password is invalid!"),
-        ("", "", "Your username is invalid!"),
+        (
+            "tomsmith",
+            "SuperSecretPassword!",
+            "You logged into a secure area!"
+        ),
+        (
+            "wronguser",
+            "wrongpass",
+            "Your username is invalid!"
+        )
     ]
 )
+def test_login_ddt(
+    page,
+    username,
+    password,
+    expected
+):
 
-def test_login_ddt(page, username, password, expected):
     login_page = LoginPage(page)
+
     login_page.open_login_page()
+
     login_page.login(username, password)
-    message = login_page.get_flash_message()
-    assert expected in message
+
+    flash_message = login_page.get_flash_message()
+
+    print("\nMessage:", flash_message)
+
+    assert expected in flash_message
