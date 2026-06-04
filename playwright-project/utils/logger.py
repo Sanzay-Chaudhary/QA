34b18cd0263b8e22afe0c7621287a83457795ab6
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-06-04 22:34:52 +0545

    logger

---
+++ playwright-project/utils/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+
+def get_logger():
+
+    logging.basicConfig(
+        filename="automation.log",
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    return logging.getLogger()
