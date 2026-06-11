3caa9a667d5d96baf08237823fb2ef96adf6aefd
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-06-11 21:21:43 +0545

    test api client

--- playwright-project/tests/test_api_client.py
+++ playwright-project/tests/test_api_client.py
@@ -5,3 +5,5 @@ client = APIClient()
 def test_get_users():
     response = client.get( "https://jsonplaceholder.typicode.com/users/1")
     assert response.status_code == 200
+
+    client.validate_status_code(response, 200)
