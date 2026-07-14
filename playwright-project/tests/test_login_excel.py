fb72019c3406fb169e60713a1be7b93e7834faee
Sanzay-Chaudhary <kctsanzu@gmail.com>
2026-07-14 21:20:36 +0545

    excel test

---
+++ playwright-project/tests/test_login_excel.py
@@ -0,0 +1,29 @@
+import pytest
+
+from utils.excel_reader import read_excel
+
+from pages.login_page import LoginPage
+
+
+test_data = read_excel(
+    "testdata/login_data.xlsx",
+    "LoginData"
+)
+
+
+@pytest.mark.parametrize(
+    "username,password",
+    test_data
+)
+
+def test_login_excel(page, username, password):
+
+    login = LoginPage(page)
+
+    login.open_login_page()
+
+    login.login(username, password)
+
+    print(
+        f"Testing {username} / {password}"
+    )
